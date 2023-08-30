4ae16d2ebf610b4cf3e285b845ef8ebc9e43e6c1
Timo Caktu <timocaktu@gmail.com>
2023-08-30 22:56:03 +0200

    thinking about adding reqeuest to skript

---
+++ src/request.rs
@@ -0,0 +1,25 @@
+/* Anfragen an Bundesamt für Bevölkerungsschutz: NINA API
+
+    Von Time Caktu
+*/
+
+//dieses File kann auch durch eine JavaScript-Datei ersetzt werden!
+
+const URL: &str = "https://warnung.bund.de/api/31";
+
+pub fn get_meldungsuebersicht(ars: String) {
+    //eine Übersicht der Meldungen in deiner Region
+    todo!()
+}
+
+pub fn get_warnungen_details(id: String) {
+    //detailinformation  zu einer Warnung!
+    todo!()
+}
+
+pub fn get_warnungen_geo(id: String) {
+    //geojson informationen zu einer Warnung!
+    todo!()
+}
+
+//usw.
